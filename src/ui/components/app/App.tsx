415865848d25ba2b258f42cd5d9572e8bf168829
myxomopx <myxomopx@gmail.com>
2020-11-04 01:46:01 +0500

    Imported @plix/core

--- src/ui/components/app/App.tsx
+++ src/ui/components/app/App.tsx
@@ -1,5 +1,11 @@
 import * as React from "react";
+import * as filters from "@plix/core/filters"
+import  parseRender from "@plix/core"
 
 export const App = () => {
-    return <>Hello, world!</>;
+    return (
+        <>
+            {Object.keys(filters).join(" ")}
+        </>
+    )
 }
