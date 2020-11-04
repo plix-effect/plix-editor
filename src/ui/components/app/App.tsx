07e341e6ec8fb0b92b00e89e364c9f0ab698a275
myxomopx <myxomopx@gmail.com>
2020-11-04 18:10:51 +0500

    Fixed imports

--- src/ui/components/app/App.tsx
+++ src/ui/components/app/App.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
-import * as filters from "@plix/core/filters"
-import  parseRender from "@plix/core"
+import * as filters from "@plix-effect/core/filters"
+import  parseRender from "@plix-effect/core"
 
 export const App = () => {
     return (
