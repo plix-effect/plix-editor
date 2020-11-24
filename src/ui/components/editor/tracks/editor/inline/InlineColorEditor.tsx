dd87ed63516ee0ca196d781d638d69f37c26d1ed
myxomopx <myxomopx@gmail.com>
2020-11-24 12:06:50 +0500

    Finished InlineColorEditor

--- src/ui/components/editor/tracks/editor/inline/InlineColorEditor.tsx
+++ src/ui/components/editor/tracks/editor/inline/InlineColorEditor.tsx
@@ -1,7 +1,11 @@
-import React, {ChangeEvent, FC, MutableRefObject, useCallback, useEffect, useRef, useState} from "react";
+import React, {ChangeEvent, FC, MutableRefObject, useCallback, useEffect, useMemo, useRef, useState} from "react";
 import "./InlineEditor.scss"
+import {HSLAColor, RGBAColor} from "@plix-effect/core/types";
+import {parseColor} from "@plix-effect/core";
 import {useInlineEditableContainer} from "../../../../../use/useInlineEditableContainer";
 import { ChromePicker } from 'react-color'
+import {colorToNumber, hslaToRgba, rgbaToNumber} from "@plix-effect/core/color";
+import Popup from "reactjs-popup";
 
 export interface InlineJsonEditorEditorProps {
     color: any,
@@ -9,40 +13,52 @@ export interface InlineJsonEditorEditorProps {
 }
 export const InlineColorEditor: FC<InlineJsonEditorEditorProps> = ({color, onChange}) => {
 
-    const [displayColorPicker, setDisplayColorPicker] = useState(false);
+    const hslaColor = useMemo(() => parseColor(color, null), [color]);
+    const styleColor = useMemo(() => getStyleColor(hslaColor), [hslaColor]);
+
+    const [colorPickerValue, setColorPickerValue] = useState(hslaToRgba(hslaColor))
 
     const handleChange = useCallback((changedColor) => {
-        console.log(changedColor.hsl);
+        console.log("change",changedColor.rgb)
+        setColorPickerValue(changedColor.rgb);
     }, [onChange]);
 
-    const handleClose = useCallback(() => {
-        setDisplayColorPicker(false);
-    }, [])
+    const submit = useCallback(() => {
+        const newColor = toSaveColor(colorPickerValue);
+        console.log("NEW COLOR",newColor,colorPickerValue)
+        onChange(newColor);
+    }, [colorPickerValue, onChange])
 
-    const handleClick = useCallback(() => {
-        setDisplayColorPicker((v) => !v);
-    }, [])
 
-    console.log("CLR",color);
-
-    const styleColor = `hsl()`
+    const trigger = useMemo(() => {
+        return (
+            <div className={"ice-color-container"} title={"Click to edit"}>
+                <div className={"ice-color"} style={{backgroundColor: styleColor}}/>
+            </div>
+        )
+    }, [hslaColor])
 
     return (
-        <form className={"inline-color-editor"}>
-            <div>
-                <div className={"swatch"} onClick={ handleClick }>
-                    <div className={"color"} style={{backgroundColor: color}}/>
-                </div>
-                {
-                    displayColorPicker ?
-                        <div className={"popover"}>
-                            <div className={"cover"} onClick={ handleClose }/>
-                            <ChromePicker color={ color } onChange={ handleChange } />
-                        </div>
-                    :
-                        null
-                }
-            </div>
-        </form>
+        <div className={"inline-color-editor"}>
+            <Popup trigger={trigger} position="bottom center" onClose={submit}>
+                <ChromePicker color={ colorPickerValue } onChange={handleChange} />
+            </Popup>
+        </div>
     );
+}
+
+function getStyleColor(color: HSLAColor) {
+    const h = color[0]*360;
+    const s = color[1]*100;
+    const l = color[2]*100;
+    const a = color[3]*100;
+    return `hsla(${h},${s}%,${l}%,${a}%)`
+}
+
+function toHtmlColor(color: HSLAColor){
+    return "#" + String((colorToNumber(color)>>>8).toString(16)).padStart(6, "0");
+}
+
+function toSaveColor(color: RGBAColor): number {
+    return rgbaToNumber(color)
 }
