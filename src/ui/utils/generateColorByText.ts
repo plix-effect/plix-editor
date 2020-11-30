e83252f7cf5432587393f5a9d46958496e3a4606
myxomopx <myxomopx@gmail.com>
2020-11-30 11:45:19 +0500

    generateColorByText now works more randomly

--- src/ui/utils/generateColorByText.ts
+++ src/ui/utils/generateColorByText.ts
@@ -3,5 +3,5 @@ export function generateColorByText(value: string, s=1, l=0.5, a=1){
     for (let i = 0; i < value.length; i++) {
         v = v + value.charCodeAt(i) * (i+1) * 9973;
     }
-    return `hsla(${v%360},${s*100}%, ${l*100}%, ${a})`;
+    return `hsla(${v%360},${40+(v*v)%61}%, ${l*100}%, ${a})`;
 }
